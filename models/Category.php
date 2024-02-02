bec6559b8fa4b44024751c5c59efe46063c78d6e
Gaellan <doucet.mari@gmail.com>
2024-02-02 11:21:43 +0100

    adding models

--- models/Category.php
+++ models/Category.php
@@ -4,8 +4,47 @@
  * @link : https://github.com/Gaellan
  */
 
-
 class Category
 {
+    private ? int $id = null;
 
+    /**
+     * @param string $name
+     */
+    public function __construct(private string $name)
+    {
+
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
--- models/Message.php
+++ models/Message.php
@@ -7,5 +7,79 @@
 
 class Message
 {
+    private ? int $id = null;
 
+    /**
+     * @param string $content
+     * @param Channel $channel
+     * @param User $user
+     */
+    public function __construct(private string $content, private Channel $channel, private User $user)
+    {
+
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return Channel
+     */
+    public function getChannel(): Channel
+    {
+        return $this->channel;
+    }
+
+    /**
+     * @param Channel $channel
+     */
+    public function setChannel(Channel $channel): void
+    {
+        $this->channel = $channel;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
 }
